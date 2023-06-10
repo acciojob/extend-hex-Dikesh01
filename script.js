f0de1664b6f861a1c3607c017c550d591134a822
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-10 12:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	  hexCode = hexCode.replace('#', '');
+
+  // Split the hex code into three color components
+  let r = hexCode[0];
+  let g = hexCode[1];
+  let b = hexCode[2];
+
+  // Repeat each color component to get the full hex code
+  let fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
+  return fullHex;
 };
 
 // Do not change the code below.
